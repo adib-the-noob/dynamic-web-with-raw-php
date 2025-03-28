d57d53dc28b5a8e709736bad04033df9f0ddec87
Mohammed Adib <mohammed.adib1104@gmail.com>
2025-03-29 01:58:13 +0600

    css condition added

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+
+    // $heading = "Home";
+    // echo "<pre>";
+    // var_dump($_SERVER);
+    // echo "</pre>";
+
+    // die();
+
     require 'views/index.blade.php';
 
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -38,12 +38,12 @@
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="/index.php" class="rounded-md px-3 py-2 text-sm font-medium text-white"
-                                aria-current="page">Dashboard</a>
-                            <a href="/about.php"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+                            <a href="/" 
+                                class="<?php if($_SERVER['REQUEST_URI'] === '/') { echo "bg-gray-900 text-white"; }?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Dashboard</a>
+                            <a href="/about"
+                                class="<?php if($_SERVER['REQUEST_URI'] === '/about') { echo "bg-gray-900 text-white"; }?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
                             <a href="/contact"
-                                class="bg-gray-900 rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                class="<?php if($_SERVER['REQUEST_URI'] === '/contact') { echo "bg-gray-900 text-white"; }?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
                             <a href="#"
                                 class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
                         </div>
